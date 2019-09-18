d59f8422a7fd3b547f15d35845d017d9dd3d24db
nhviet90 <soul20290@gmail.com>
2019-09-18 13:01:27 +0700

    new login.html edit js

--- Login/js/main.js
+++ Login/js/main.js
@@ -1,11 +1,7 @@
 //event ~ Sự kiện event base language
 //DOM
-window.onload = init;
-
-function init() {
-    let inputDom = document.getElementById("input-first-name");
-    console.log(inputDom);
-    inputDom.style.background = 'red';
+function register() {
+    
 }
 
 //hoàn thiện trang đăng nhập 
